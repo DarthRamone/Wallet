19fd7bc7be534af31bef397605ef2c1667a00c7f
Aleksandr Moshkin <moshkin_alexander@outlook.com>
2017-03-03 10:09:36 +0300

    Balance cell added

---
+++ Wallet.iOS/Views/Widgets/Balance/BalanceWidget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Wallet.iOS {
+  public partial class BalanceWidget : UICollectionViewCell {
+    public static readonly NSString Key = new NSString("BalanceWidget");
+    public static readonly UINib Nib;
+
+    static BalanceWidget() {
+      Nib = UINib.FromName("BalanceWidget", NSBundle.MainBundle);
+    }
+
+    protected BalanceWidget(IntPtr handle) : base(handle) {
+      // Note: this .ctor should not contain any initialization logic.
+    }
+  }
+}
---
+++ Wallet.iOS/Views/Widgets/Balance/BalanceWidget.designer.cs
@@ -0,0 +1,24 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using UIKit;
+
+namespace Wallet.iOS {
+  [Register("BalanceWidget")]
+  partial class BalanceWidget {
+
+    [Outlet]
+    private UILabel TitleLabel { get; set; }
+
+    [Outlet]
+    private UILabel BalanceLabel { get; set; }
+
+
+    void ReleaseDesignerOutlets() {
+    }
+  }
+}
